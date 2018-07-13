68155c8ff157a3d47ed73e4f30ddb501230d3e35
Sagi Bazinin <sagibaz@post.bgu.ac.il>
2018-07-13 17:38:28 +0300

    init

---
+++ TestPlanner/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestPlanner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestPlanner")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f058024-b5fe-43d4-9386-d6461f0903ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestPlanner/TestParse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IMAP.General;
+using System.IO;
+using IMAP.SDRPlanners;
+
+namespace TestPlanner
+{
+    [TestClass]
+    public class TestParse
+    {
+        [TestMethod]
+        public void TestParseDomain()
+        {
+            string filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName + @"\TestFiles\B3\d.pddl";
+            Domain d = Parser.ParseDomain(filePath, "agent");
+            Assert.AreEqual(4, d.Actions.Count);
+            Assert.AreEqual(11, d.Constants.Count);
+        }
+    }
+}
